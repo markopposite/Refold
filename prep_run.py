6d294fbf83ba6a8f8f6278b7d57cdf98b656634d
markopposite <markwpoe@uwm.edu>
2017-07-12 01:51:19 -0500

    UP to date version of prep_run.py wit hcommand line option to enter .csv filename

--- prep_run.py
+++ prep_run.py
@@ -1,15 +1,22 @@
 ### Script for refolding pulsar candidates in the GBNCC folder
 ### __author(s): Mark Poe, Kaleb Maraccini __
 
-#### Import relevant modules for the script
+### Takes .csv file name as command line option; 'python script.py spreadsheet.csv'
+
 from collections import defaultdict
 from commands import *
 from glob import glob
 import csv, os, sys
 
+### Define the current and parent directories
+direct = os.getcwd()
+parent = os.path.abspath(os.path.join(direct, os.pardir))
+# print parent
+# print direct
+
 ### Define and separate the ID column from a csv file into a list
 columns = defaultdict(list)
-with open('refold_cand.csv') as f:
+with open('%s' % sys.argv[1]) as f:
     reader = csv.reader(f)
     reader.next()
     for row in reader:
@@ -23,12 +30,12 @@ str2 = '*'
 new_csv_list = [str1 + x + str2 for x in csv_list]
 
 ##################################################################################
-### Create list, a dictionary, and output files from parent directory using ID's ###
+######### Create lists and a dictionary from parent directory using ID's #########
 ##################################################################################
 
 ### Begin operations in parent directory
 last_dir = ''
-dir = r'//lustre/cv/projects/GBNCC/'
+dir = parent
 os.chdir(dir)
 
 ### Create a list of the .fits filetypes and output the .fits filenames (choose 'print')
@@ -37,16 +44,28 @@ for item in new_csv_list:
     filenames = getoutput('ls -r' +' '+ item)
     l.append(filenames)
 
-### Create a dictionary ---> "filename(from csv)":"filename(corresponding .fits file)"
-dict1 = {}
+### Handle any items in l that have two instances, and place them in a list within l
+grange = []
+for jangle in l:
+	if isinstance(jangle, basestring):
+		jerk = jangle.split('\n')
+		grange.append(jerk)
+
+### All items from l including sublist items are in flatlist separately
+flatlist = []
+for sublist in grange:
+	for item in sublist:
+		flatlist.append(item)
+
+### Create a dictionary ---> {(filename(from csv)):(.fits,id,period,pd,dm)}
 dict2 = {}
 period = columns[3]
 pd = columns[4]
 dm = columns[5]
 filename = columns[0]
 for id in range(len(filename)):
-    dict1[filename[id]] = l[id]
-    dict2[filename[id]] = [l[id],csv_list[id],period[id],pd[id],dm[id]]
+    dict2[filename[id]] = [grange[id],csv_list[id],period[id],pd[id],dm[id]]
+# print dict2
 
 ### End operations in parent directory
 dir = os.getcwd()
@@ -54,72 +73,101 @@ os.chdir('..')
 last_dir = os.getcwd()
 
 ##################################################################################
-### Build subdirectories with csv filenames and parse strings for rfi command ####
+### Build subdirectories with csv filenames and parse strings for command line ###
 ##################################################################################
 
 ### Create subdirectories for each corresponding "C1234+56" filename
 try:
-	basedir = "/lustre/cv/projects/GBNCC/refold/"
+	basedir = direct
 	for id in filename:
 		os.mkdir(os.path.join(basedir,id))
 except:
 	pass
 
 ### Create a list of the "C1234+56" subdirectories that are in the current directory
-paths = glob('/lustre/cv/projects/GBNCC/refold/*/')
+paths = glob(direct +'/*/')
 paths = [p[:-1] for p in paths]
 	
-### Define new variable for rfifind string input 
+### Split strings appropriately for use in rfifind and prepfold commands
 vox = []
 vac = []
-for str in l:
-	c = str.split('/')
-	d = c[1].split('.')
-	e = d[0].split('_0001')
-	f = e[0].split('_2bit')
-	jag = f[0]
-	grr = e[0]
-	if '_2bit' in e[0]:
-		v = jag
-	else:
-		v = grr
-	vox.append(v)
-	vac.append(c[1])	
+for quarrel in grange:
+	for str in quarrel:
+		c = str.split('/')
+		d = c[1].split('.')
+		e = d[0].split('_0001')
+		f = e[0].split('_2bit')
+		jag = f[0]
+		grr = e[0]
+		if '_2bit' in e[0]:
+			v = jag
+		else:
+			v = grr
+		vox.append(v)
+		vac.append(c[1])	
+# print vox
+# print vac
 
 ##################################################################################
-###### Build a system to run 'rfifind' and 'prepfold' in subdirectories #########
+####### Build a system to run 'rfifind' and 'prepfold' in subdirectories #########
 ##################################################################################
 
-### Perform soft link, and rfifind in the corresponding subdirectories
+### Change directory to corresponding C1234+56 sub-directory
 for item in paths:
 	curr = item
 	os.chdir(curr)
+	### Set environment variables
 	cmd1 = ('. /users/sransom/bin/zuul_envs.sh')
 	os.system(cmd1) # env variables for GBNCC shell
+	
+	### Create soft link, if already exists, move on to next one
+	### Check that the filenames match the split strings and dictionary values
 	try:
-		for key in dict1:
-			for vic in vac:
-				if key in item: 
-					if vic in dict1[key]:	
-						src = os.path.join('/lustre/cv/projects/GBNCC/',dict1[key])
-						dst = os.path.join('/lustre/cv/projects/GBNCC/refold/',key,vic)
-						os.symlink(src,dst)
+		for key in dict2:
+			for fire in dict2[key][0]:
+				for vic in vac:
+					if key in item: 
+						if vic in fire:	
+							### Soft link
+							src = os.path.join(parent,fire)
+							dst = os.path.join(direct,key,vic)
+							print src
+							print dst
+							os.symlink(src,dst)
 	except:
 		pass
-	for var in vox:
-		for thing in vac:
-			if thing in src:
-				if var in src:
-					os.system('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
-					#print ('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
 
-	for thing in vac:
-		for var in vox:
-			if thing in src:
-				if var in src:					
-					os.system('prepfold -p ' + dict2[key][2] + ' -pd ' + dict2[key][3] + ' -dm ' + 
-							dict2[key][4] + ' -n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
-							'-mask ' + var + '_rfifind.mask' + ' ' + thing)
+	### Create list of items in current subdirectory
+	doodad = []
+	for beer in os.listdir(item):
+		doodad.append(beer)
+	
+	### Check that for all instances of the filename, they match and run rififind
+	for var in doodad:
+		for bug in flatlist:
+			if var in bug:
+				for thing in vac:
+					if thing in bug:
+						if var in bug:
+							os.system('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
+# 							print ('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
+
+	### Check that for all instances of the filename, they match and run prepfold
+	for var in doodad:
+		for bug in flatlist:
+			if var in bug:
+				for thing in vac:
+					if thing in bug:
+						if var in bug:
+							os.system('prepfold -p ' + dict2[key][2] + '-pd ' + dict2[key][3] + ' -dm ' + 
+									dict2[key][4] + '-n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
+									'-mask ' + var +'_rfifind.mask' + ' ' +thing)
+# 							print ('prepfold -p ' + dict2[key][2] + '-pd ' + dict2[key][3] + ' -dm ' + 
+# 									dict2[key][4] + '-n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
+# 									'-mask ' + var +'_rfifind.mask' + ' ' +thing) 
 	dir = os.getcwd()
 	os.chdir('..')
-print 'done'
+
+
+
+print '\n\n\nFINISHED'
