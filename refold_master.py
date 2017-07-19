9995e0fa7a73624f666193c9e2041710f774967e
markopposite <markwpoe@uwm.edu>
2017-07-18 23:06:11 -0500

    Update refold_master.py

--- refold_master.py
+++ refold_master.py
@@ -1,5 +1,5 @@
 ### Script for refolding pulsar candidates in the GBNCC folder
-### __author(s)__   Mark Poe, Kaleb Maraccini  __
+### __author(s): Mark Poe, Kaleb Maraccini __
 
 ### Takes .csv file name as command line option; 'python script.py spreadsheet.csv'
 
@@ -11,6 +11,38 @@ import csv, os, sys
 ### Define the current and parent directories
 direct = os.getcwd()
 parent = os.path.abspath(os.path.join(direct, os.pardir))
+
+msg =  """	
+#####################################################################################
+########################## Library of optional arguments ############################
+#####################################################################################
+
+### Arguments can be between 2 and 4 total, examples include:
+
+user$ 'python script.py sheet.csv' 
+# Will prompt to overwrite original sheet
+
+user$ 'python script.py sheet.csv newsheet.csv'
+# Will create a new spreadsheet w/ y/n column updated
+
+user$ 'python script.py sheet.csv Users/name/directoryfor_png/'
+# Will propmt to overwrite csv, or create a new one, and copy png's to a specified folder
+
+user$ 'python script.py sheet.csv newsheet.csv Users/name/directoryfor_png/'
+# No prompts, create updated spreadsheet, and copy png's to specified folder 
+
+#####################################################################################
+#####################################################################################
+"""	
+
+### Determine if a useable number of arguments were given
+if len(sys.argv) <= 1:
+	print 'Inadequate number of arguments were given, here are the possible options: '
+	print msg
+	exit()
+elif len(sys.argv) >= 5:
+	print msg	
+	exit()
 
 ### Define and separate the ID column from a csv file into a list
 infile = '%s' % sys.argv[1]
@@ -89,28 +121,31 @@ paths = glob(direct + '/*/')
 paths = [p[:-1] for p in paths]
 
 ### Split strings appropriately for use in rfifind and prepfold commands
-vox = []
+# vox = []
 vac = []
 for quarrel in grange:
 	for str in quarrel:
 		c = str.split('/')
-		d = c[1].split('.')
-		e = d[0].split('_0001')
-		f = e[0].split('_2bit')
-		jag = f[0]
-		grr = e[0]
-		if '_2bit' in e[0]:
-			v = jag
-		else:
-			v = grr
-		vox.append(v)
-		vac.append(c[1])
+		for i in c:
+			if i.endswith('.fits'):
+				vac.append(c[1])
+vac_1 = []
+for i in vac:
+  if i not in vac_1:
+    vac_1.append(i)
+
+
+
+# print "VOX (parsed strings for rfifind)PARSED STRING FOR RFI FIND"
+# print vox[:3]
 
 ##################################################################################
 ###### Build a system to run 'rfifind' and 'prepfold' in subdirectories #########
 ##################################################################################
 
 print '\n...\n'
+### print y/n column from infile
+# print columns[6]
 
 ### Change directory to corresponding C1234+56 sub-directory
 for item in paths:
@@ -118,73 +153,117 @@ for item in paths:
 	os.chdir(curr)
 	### Set environment variables
 	cmd1 = ('. /users/sransom/bin/zuul_envs.sh')
-# 	os.system(cmd1)
+	os.system(cmd1)
 	
-	### Create soft link, if already exists, move on to next one
-	try:
-		### Check that the filenames match the split strings and dictionary values
-		for key in dict2:
-			for fire in dict2[key][0]:
-				for vic in vac:
-					if key in item: 
-						if vic in fire:	
-							### Soft link
-							src = os.path.join(parent,fire)
-							dst = os.path.join(direct,key,vic)
-							os.symlink(src,dst)
-	except:
-		pass
-	
-	### Create list of items in current subdirectory
-	doodad = []
-	for beer in os.listdir(item):
-		doodad.append(beer)
+	for key in dict2:
+		if key in item:
+			if dict2[key][5] is 'y':
+				continue
+			elif dict2[key][5] is 'n':
+			### Create soft link, if already exists, move on to next one
+				try:
+					### Check that the filenames match the split strings and dictionary values
+# 						for key in dict2:
+					for fire in dict2[key][0]:
+						for vic in vac:
+							if key in item: 
+								if vic in fire:	
+									### Soft link
+									src = os.path.join(parent,fire)
+									dst = os.path.join(direct,key,vic)
+									os.symlink(src,dst)
+			
+				except:
+					pass
 
-	### Check 'Ran'	column to see if candidate has been processed already	
-	for i in dict2[key][5]:
-		if i is 'n':
-			### Check that for all instances of the filename, they match and then run rififind, and prepfold
-			for var in doodad:
-				for bug in flatlist:
-					for thing in vac:							
-						if var in bug:
-							if thing in bug:
-								os.system('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
-								os.system('prepfold -p ' + dict2[key][2] + ' -pd ' + dict2[key][3] + ' -dm ' + 
-										dict2[key][4] + ' -n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
-										'-mask ' + var +'_rfifind.mask' + ' ' +thing)
-# 								print ('rfifind -o '+ var + ' -time 1.0 -timesig 3.0 ' + thing)
+					### Create list of items in current subdirectory
+					doodad = []
+					for beer in os.listdir(item):
+						doodad.append(beer)
+
+					### Check if 'Processed' column is Y/N
+				# 	if i in columns[6] is 'n':
+
+					###Check that for all instances of the filename, they match and then run prepfold
+					for var in doodad:
+						for bug in vac_1:
+							if var in bug:
+								j = bug.split('.')
+								g = j[0].split('_0001')
+								h = g[0].split('_2bit')
+# 									print h[0]
+# 								print bug
+								os.system('rfifind -o '+ h[0] + ' -time 1.0 -timesig 3.0 ' + bug)
+								os.system('prepfold -p ' + dict2[key][2] + '-pd ' + dict2[key][3] + ' -dm ' + 
+											dict2[key][4] + '-n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin '
+											'-mask ' + h[0] +'_rfifind.mask' + ' ' +bug) 
+# 								print ('rfifind -o '+ h[0] + ' -time 1.0 -timesig 3.0 ' + bug)
 # 								print ('prepfold -p ' + dict2[key][2] + '-pd ' + dict2[key][3] + ' -dm ' + 
-# 											dict2[key][4] + '-n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
-# 											'-mask ' + var +'_rfifind.mask' + ' ' +thing) 
+# 										dict2[key][4] + '-n 128 -nsub 128 -npart 60 -fine -nosearch -noxwin ' 
+# 										'-mask ' + h[0] +'_rfifind.mask' + ' ' +bug) 
+								
+								
+								
 								for i in dict2[key][5]:
 									dict2[key][5] = 'y'
+								continue
 
 	dir = os.getcwd()
 	os.chdir('..')
 
-### Copy all .png files to User home folder
-os.system('cp C*/*.png /users/kmaracci/refold')
-# 	os.system('C*/*.png /users/mpoe/refold')
+#####################################################################################
+########################## Library of optional arguments ############################
+#####################################################################################
 
-### Update the csv to indicate if specific candidates have been refolded
-with open(infile,'wb') as x:
+if len(sys.argv) == 2:
+	checkit = raw_input('overwrite csv file? y/n: ')
+	if checkit is 'y':
+		csv_file = sys.argv[1]
+	elif checkit is 'n':
+		namefile = raw_input('create a .csv filename (include ".csv"): ')
+		os.system('touch '+namefile)
+		csv_file = namefile
+	else:	
+		exit()
+# 	os.system('cp C*/*.png /users/kmaracci/refold')			
+
+elif len(sys.argv) > 2 and '.csv' in sys.argv[2]:
+	csv_file = sys.argv[2]
+# 	os.system('cp C*/*.png /users/kmaracci/refold')
+
+elif len(sys.argv) > 2 and '/' in sys.argv[2]:
+	csv_file = sys.argv[1]
+	os.system('cp C*/*.png %s' % (sys.argv[3]))
+	checkit = raw_input('overwrite csv file? y/n: ')
+	if checkit is 'y':
+		csv_file = sys.argv[1]
+	elif checkit is 'n':
+		namefile = raw_input('create a .csv filename (include ".csv"): ')
+		os.system('touch '+namefile)
+		csv_file = namefile
+	else:	
+		exit()
+
+elif len(sys.argv) > 3 and '/' in sys.argv[3]:
+	csv_file = sys.argv[2]
+	os.system('cp C*/*.png %s' % (sys.argv[3]))
+
+else:
+	print 'Error: incorrect syntax or number of arguments'
+# print csv_file
+
+#######################################
+#### 'Processed' y/n column update ####
+#######################################
+
+### update csv file to indicate if candidates are procesed
+with open(csv_file,'wb') as x:
 	writer = csv.writer(x)
 	writer.writerow(['Filename,ID,Period (bary),Period (s),PD,DM,Processed'])
 	for key, value in dict2.items():
 		writer.writerow([key, dict2[key][1],dict2[key][6],dict2[key][2],dict2[key][3],
 						dict2[key][4],dict2[key][5]])
-
-### Enter two csv filenames as options, and the second one will be a copy of the first
-### For testing and troubleshooting only
-# outfile = '%s' % sys.argv[2]
-# f1 = open(infile)
-# with open(outfile,'wb') as f:
-# 	writer = csv.writer(f)
-# 	with open (infile,'r') as x:
-# 		reader = csv.reader(x, delimiter = ',')
-# 		for row in reader:
-# 			writer.writerow(row)
      	 	
-print '...'
+print '\n...'
 print '\n\n\nFINISHED'
+
